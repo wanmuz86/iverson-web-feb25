3d62f5dad9e214ade5027b09c9239e7684144e40
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-06 16:06:46 +0800

    add DOM Manipulation on the table

--- project/list.js
+++ project/list.js
@@ -8,17 +8,46 @@ document.addEventListener("DOMContentLoaded", function(){
     .then(response=> response.json())
     .then(data => {
         console.log(data)
-        // Do for loop -
+
+        // Verify with your API / Sheety data
+        const employees = data.employees;
+
+        // For loop to create the rows (5)
+        for (let i = 0; i < employees.length; i++){
+
             // Create new row tr
+            let newTr = document.createElement("tr");
 
-            // Create new column ? How many columns are there
+            // Verify on your table, how many columns are there - 4
+            // Create new column ? 4 columns
+            let newTd1 = document.createElement("td");
+            let newTd2 = document.createElement("td");
+            let newTd3 = document.createElement("td");
+            let newTd4 = document.createElement("td");
 
-            // Set the innerText or HTML of the column
+             // Set the  innerHTML of the column
+             newTd1.innerHTML = employees[i].empid;
+             newTd2.innerHTML = employees[i].name;
+             newTd3.innerHTML = employees[i].department;
+             // Homework, for those who want to do it, add the button - View, Edit, Delete
 
-            // Add the Columns to The row
+             // Create 3 buttons
+             // Add class to the button "btn btn-primary" , "btn btn-secondary" , "btn btn-danger"
+             // AddEventListener "click" to each of the button
+             // Append to newTd
+             newTd4.innerHTML = "TODO BUTTONS LATER!!"
 
-
-            // Add the rows to the body
+             // Add the Columns to The row
+             newTr.appendChild(newTd1);
+             newTr.appendChild(newTd2);
+             newTr.appendChild(newTd3);
+             newTr.appendChild(newTd4);
+             
+             // Add the rows to the tbody
+             let tbody = document.getElementById("employee_tbody");
+             tbody.appendChild(newTr)
+        }
+   
     })
     .catch(err=> console.log(err))
    }
